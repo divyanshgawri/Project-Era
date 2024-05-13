fda747d671ada8b7ef3f5c2a36c572859ab43d5f
divyanshgawri <dishugawri@gmail.com>
2024-05-14 00:53:19 +0530

    dn

--- bootstrap/projectera/projectera_main/models.py
+++ bootstrap/projectera/projectera_main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 class Projects(models.Model):
     name = models.CharField(max_length=30)
     title = models.CharField(max_length=40)
--- bootstrap/projectera/projectera_main/views.py
+++ bootstrap/projectera/projectera_main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+from .models import Projects
 def index(request):
     return render(request,'index.html')
 def python(request):
@@ -6,5 +7,22 @@ def python(request):
 def proj1python(request):
     return render(request,'project1.html')
 def upload_proj1(request):
+    if request.method == "POST":
+        user_name = request.POST.get('user_name')
+        proj_title= request.POST.get('proj_title')
+        proj_desc = request.POST.get('proj_desc')
+        # choose = request.POST.get('choose')
+        image1 = request.POST.get('image1')
+        image2 = request.POST.get('image2')
+        project_zip = request.POST.get('project_zip')
+        p1 = Projects()
+        p1.names = user_name
+        p1.titles = proj_title
+        p1.desc = proj_desc
+        p1.images1 = image1
+        p1.images2 = image2
+        p1.zipi = project_zip
+        p1.save()
+        redirect("trr")
     return render(request,'upload.html')
 # Create your views here.
