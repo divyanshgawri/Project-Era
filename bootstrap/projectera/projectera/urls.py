7fe0c6050101eb325973338f1885b5ff37e124fe
divyanshgawri <dishugawri@gmail.com>
2024-05-13 20:21:46 +0530

    dn

--- bootstrap/projectera/projectera/urls.py
+++ bootstrap/projectera/projectera/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from projectera_main import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('',views.index,name='index'),
     path('trr',views.python,name='trr'),
-    path('proj1',views.proj1python,name='proj1')
+    path('proj1',views.proj1python,name='proj1'),
+    path('upload_proj',views.upload_proj1,name='upload_proj')
 ]
--- bootstrap/projectera/projectera_main/views.py
+++ bootstrap/projectera/projectera_main/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 def index(request):
     return render(request,'index.html')
 def python(request):
     return render(request,'python.html')
 def proj1python(request):
     return render(request,'project1.html')
+def upload_proj1(request):
+    return render(request,'upload.html')
 # Create your views here.
